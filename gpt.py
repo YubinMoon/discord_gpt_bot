f7b2324a30754017c920f6ffa1dcf6555cec2c44
terry <munu220@gmail.com>
2023-06-14 14:17:36 +0900

    GPTSetting class 생성

--- gpt.py
+++ gpt.py
@@ -12,10 +12,60 @@ import io
 from dotenv import load_dotenv
 
 
+class GPTSetting:
+    def __init__(self, file_name: str = "setting.json"):
+        self.file_name = file_name
+        self.setting_type = {
+            "model": str,
+            "system_text": str,
+            "max_token": int,
+            "temperature": float,
+            "top_p": float,
+            "keep_min": int,
+        }
+        self.setting_value = {
+            "model": "gpt-3.5-turbo-0613",
+            "system_text": "",
+            "max_token": 4000,
+            "temperature": 1.0,
+            "top_p": 1.0,
+            "keep_min": 10,
+        }
+        self.load_from_json()
+        self.sync_setting()
+
+    def load_from_json(self):
+        if os.path.isfile(self.file_name):
+            loaded_settings = json.load(open(self.file_name, "r", encoding="utf-8"))
+            self.load_from_dict(loaded_settings)
+
+    def load_from_dict(self, settings: dict):
+        for key in self.setting_value.items():
+            self.setting_value = settings.get(key, self.setting_value[key])
+
+    def set_setting(self, key: str, value: str):
+        if key not in self.setting_value:
+            raise KeyError(f"'{key}'이라는 설정은 존재하지 않습니다.")
+        self.setting_value[key] = self.setting_type[key](value)
+        self.save_to_json()
+        self.sync_setting()
+
+    def save_to_json(self):
+        json.dump(self.setting_value, open(self.file_name, "w", encoding="utf-8"))
+
+    def sync_setting(self):
+        self.model = self.setting_value["model"]
+        self.system_text = self.setting_value["system_text"]
+        self.max_token = self.setting_value["max_token"]
+        self.temperature = self.setting_value["temperature"]
+        self.top_p = self.setting_value["top_p"]
+        self.keep_min = self.setting_value["keep_min"]
+
+
 class GPT:
-    def __init__(self, setting: str = "setting.json"):
+    def __init__(self, api_key: str, setting_file: str = "setting.json"):
         load_dotenv()
-        self.setting_file = setting
+        self.setting_file = setting_file
         self.gloSetting = {}
         self.logger = logging.getLogger(__name__)
         self.lastRequestTime = time.time()
