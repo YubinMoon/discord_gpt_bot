c40942bda3e7c4eeb55dd171c29886d378f2189e
terry <munu220@gmail.com>
2023-06-23 22:06:50 +0900

    token 테스트 추가

--- test/GPT/token.py
+++ test/GPT/token.py
@@ -1,36 +1,36 @@
-import os
-from unittest import TestCase, IsolatedAsyncioTestCase
-from GPT import chat
+import unittest
 from GPT import setting
-from GPT import gpt
 from GPT import token
 from GPT import message
 
 
-class TokenTests(TestCase):
+class TokenTests(unittest.TestCase):
     data1 = {"delta": {"role": "user", "content": "안녕? "}, "finish_reason": "null"}
     data2 = {"delta": {"role": "user", "content": "아이 졸려"}, "finish_reason": "stop"}
     data3 = {"delta": {"role": "user", "content": "안녕? 아이 졸려"}, "finish_reason": "stop"}
 
     def setUp(self):
         self.setting = setting.Setting()
-        msg1 = message.AssistanceMessage(data=self.data1)
-        msg2 = message.AssistanceMessage(data=self.data2)
-        msg3 = message.AssistanceMessage(data=self.data3)
+        self.msg1 = message.AssistanceMessage(data=self.data1)
+        self.msg2 = message.AssistanceMessage(data=self.data2)
+        self.msg3 = message.AssistanceMessage(data=self.data3)
         self.msg_box = message.MessageBox()
-        self.msg_box.add_message(msg1)
-        self.msg_box.add_message(msg2)
-        self.msg_box.add_message(msg3)
-        self.message = self.msg_box.make_messages(setting=self.setting)
-
-        self.assertEqual(self.setting.system_text, "")
-        self.assertEqual(self.setting.max_token, 3000)
-        self.assertEqual(self.setting.temperature, 1.0)
-        self.assertEqual(self.setting.top_p, 1.0)
-        self.assertEqual(self.setting.keep_min, 10)
 
     def test_get_token(self):
+        self.msg_box.add_message(self.msg1)
+        self.msg_box.add_message(self.msg2)
+        self.message = self.msg_box.make_messages(setting=self.setting)
         token_num = token.Tokener.num_tokens_from_messages(
             self.message, self.setting.model
         )
-        self.assertEqual(token_num, 47)
+        self.assertEqual(token_num, 25)
+        self.msg_box.add_message(self.msg3)
+        self.message = self.msg_box.make_messages(setting=self.setting)
+        token_num = token.Tokener.num_tokens_from_messages(
+            self.message, self.setting.model
+        )
+        self.assertEqual(token_num, 42)
+
+
+if __name__ == "__main__":
+    unittest.main()
